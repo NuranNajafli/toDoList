19a7b5370b244a01c97e79ae69a4ac1ef7c324ed
NuranNajafli <nnajafli2021@ada.edu.az>
2023-06-04 18:36:44 +0400

    To do list without css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [values, setValues] = useState("");
+  const [listItem, setListItem] = useState([]);
+
+  const addItem = () => {
+    if (!values) {
+      alert("please enter something");
+    }
+
+    const item = {
+      id: listItem.length + 1,
+      value: values,
+    };
+    setListItem((pre) => [...pre, item]);
+    setValues("");
+  };
+
+  const deleteItem = (id) => {
+    const newList = listItem.filter((a) => a.id !== id);
+    setListItem(newList);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <h1>To do List</h1>
+      <div className="event">
+        <input
+          type="text"
+          placeholder="write something"
+          value={values}
+          onChange={(e) => setValues(e.target.value)}
+        />
+        <button onClick={addItem}>Add</button>
+      </div>
+      <ul>
+        {listItem.map((e, i) => (
+          <li key={i}>
+            {e.value} <button onClick={() => deleteItem(e.id)}>X</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
